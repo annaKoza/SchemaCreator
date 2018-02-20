8dbb33882e118e49d2b881359ec67ca22d3a9391
Anna Kozłowska <a.kozlowska@prevac.pl>
2018-02-20 15:11:42 +0100

    Added: DrawAdorner

---
+++ SchemaCreator.Designer/Adorners/DrawAdorner.cs
@@ -0,0 +1,63 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using SchemaCreator.Designer.Controls;
+
+namespace SchemaCreator.Designer.AttachedProperties
+{
+    internal class DrawAdorner : Adorner
+    {
+        private Canvas _itemsPanel;
+        private Point? _selectionStartPoint;
+        private ISelectionPanel _designerPanel;
+        private Point? _endPoint;
+
+        public DrawAdorner(Canvas itemsPanel, Point? selectionStartPoint, ISelectionPanel designerPanel) : base(itemsPanel)
+        {
+            _itemsPanel = itemsPanel;
+            _selectionStartPoint = selectionStartPoint;
+            _designerPanel = designerPanel;
+        }
+
+        protected override void OnMouseMove(MouseEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                if (!IsMouseCaptured)
+                    CaptureMouse();
+
+                _endPoint = e.GetPosition(this);
+                InvalidateVisual();
+            }
+            else
+            {
+                if (IsMouseCaptured) ReleaseMouseCapture();
+            }
+
+            e.Handled = false;
+        }
+
+        protected override void OnPreviewMouseUp(MouseButtonEventArgs e)
+        {
+            if (IsMouseCaptured) ReleaseMouseCapture();
+            var adornerLayer = AdornerLayer.GetAdornerLayer(_itemsPanel);
+            adornerLayer?.Remove(this);
+
+           e.Handled = false;
+        }
+
+        protected override void OnRender(DrawingContext dc)
+        {
+            base.OnRender(dc);
+
+            // without a background the OnMouseMove event would not be fired!
+            dc.DrawRectangle(Brushes.Transparent, null, new Rect(RenderSize));
+            if (_selectionStartPoint.HasValue && _endPoint.HasValue) {
+               dc.DrawLine(new Pen(new SolidColorBrush(Colors.Black), 2), _selectionStartPoint.Value, _endPoint.Value);
+            }
+        }
+
+    }
+}
--- SchemaCreator.Designer/AttachedProperties/DrawAttachedProperty.cs
+++ SchemaCreator.Designer/AttachedProperties/DrawAttachedProperty.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using SchemaCreator.Designer.Controls;
 using SchemaCreator.Designer.Helpers;
 using SchemaCreator.Designer.Interfaces;
 
@@ -34,11 +36,30 @@ namespace SchemaCreator.Designer.AttachedProperties
             {
                 designer.Loaded -= OnDesignerLoaded;
                 designer.MouseLeftButtonDown -= OnDesignerStartDraw;
-                designer.MouseLeftButtonUp -= OnDesignerStopDraw;
+                designer.PreviewMouseLeftButtonUp -= OnDesignerStopDraw;
+                designer.MouseMove -= OnDesignerContinueDraw;
             }
             designer.Loaded += OnDesignerLoaded;
             designer.MouseLeftButtonDown += OnDesignerStartDraw;
-            designer.MouseLeftButtonUp += OnDesignerStopDraw;
+            designer.MouseMove += OnDesignerContinueDraw;
+            designer.PreviewMouseLeftButtonUp += OnDesignerStopDraw;
+        }
+
+        private static void OnDesignerContinueDraw(object sender, MouseEventArgs e)
+        {
+            if (e.LeftButton != MouseButtonState.Pressed)
+                _selectionStartPoint = null;
+
+            if (_selectionStartPoint.HasValue)
+            {
+                var adornerLayer = AdornerLayer.GetAdornerLayer(_itemsPanel);
+                if (adornerLayer != null)
+                {
+                    var adorner = new DrawAdorner(_itemsPanel, _selectionStartPoint, _designerPanel);
+                    adornerLayer.Add(adorner);
+                }
+            }
+            e.Handled = true;
         }
 
         private static IDesignerViewModel _designerViewModel;
@@ -47,7 +68,10 @@ namespace SchemaCreator.Designer.AttachedProperties
         {
             var itemsPresenter = (sender as Designer).GetVisualChild<ItemsPresenter>();
             _itemsPanel = VisualTreeHelper.GetChild(itemsPresenter, 0) as Canvas;
-
+            var itemsControl = (sender as Designer).GetVisualChild<ItemsControl>();
+            _designerPanel = itemsControl is ISelectionPanel
+                ? itemsControl as ISelectionPanel
+                : throw new ArgumentException("ItemsControl must implement ISelectionPanel interface");
             var dataContext = (sender as Designer).DataContext;
             _designerViewModel = dataContext as IDesignerViewModel ?? throw new ArgumentException("datacontext must implement IDesignerViewModel interface");
         }
@@ -75,15 +99,18 @@ namespace SchemaCreator.Designer.AttachedProperties
         }
 
         private static IDrawableItem _drawableInstance;
+        private static Point? _selectionStartPoint;
+        private static ISelectionPanel _designerPanel;
+
         private static void OnDesignerStartDraw(object sender, MouseButtonEventArgs e)
         {
             if (_designerViewModel.ItemToDraw == null) return;
             _drawableInstance = Activator.CreateInstance(_designerViewModel.ItemToDraw.GetType()) as IDrawableItem;
             if(_drawableInstance == null) return;
             
-            var position = e.GetPosition(_itemsPanel);
-            _drawableInstance.X1 = position.X;
-            _drawableInstance.Y1 = position.Y;
+            _selectionStartPoint = e.GetPosition(_itemsPanel);
+            _drawableInstance.X1 = _selectionStartPoint.Value.X;
+            _drawableInstance.Y1 = _selectionStartPoint.Value.Y;
             e.Handled = false;
         }
         
